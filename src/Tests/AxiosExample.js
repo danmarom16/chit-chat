c288642947dcc7c8259d85fe66257f543b48994e
Asaf Mesilaty <asafmcs8@gmail.com>
2022-05-17 13:59:23 +0300

    async getData with catch

--- src/Tests/AxiosExample.js
+++ src/Tests/AxiosExample.js
@@ -5,20 +5,30 @@ function AxiosExample() {
     const [data, setData] = useState("");
     const [count, setCount] = useState(0);
 
-    const getData = () => {
-        Axios.get("https://reqres.in/api/users/2").then(
-            (response) => {
-                console.log(response.data);
-                setData(JSON.stringify(response.data));
-                setCount(() => (count+1));
-            } 
-        )
+    const handleClick = () => {
+      getData();
+      setCount(() => (count+1));
+      console.log(data);
     }
+
+    async function getData() {
+      try {
+         let res = await Axios.get("https://reqres.in/api/users/2");
+          if(res.status == 200){
+              // test for status you want, etc
+              console.log(res.status)
+              setData(JSON.stringify(res.data));
+          }    
+      }
+      catch (err) {
+          console.error(err);
+      }
+  }
 
   return (
     <div>
     <h1>AxiosExample</h1>
-    <button onClick={getData}> Click to make get Req</button> 
+    <button onClick={handleClick}> Click to make get Req</button> 
     <h2>{count}</h2>
     <h3>{data[count]}</h3>   
     </div>
